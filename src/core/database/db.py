5e6643dc1de5544335811b507fe1e971ca1b2cfc
Marsel <buzzakka6@gmail.com>
2024-04-23 18:18:53 +0300

    feat: добавил файл подключения к базе

---
+++ src/core/database/db.py
@@ -0,0 +1,19 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+
+from src.core.config import settings
+
+async_engine = create_async_engine(
+    url=settings.postgres_settings.get_pg_url(),
+)
+
+async_session_maker = async_sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
