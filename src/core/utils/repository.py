19bf061cf633b8d5fb9f696d8947fae75098781a
Marsel <buzzakka6@gmail.com>
2024-04-23 22:29:29 +0300

    feat: добавил метод update_one_or_create_new в репозиторий и сервис

--- src/core/utils/repository.py
+++ src/core/utils/repository.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from sqlalchemy import Result, select
+from sqlalchemy import Result, insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -8,6 +8,10 @@ class AbstractRepository(ABC):
     
     @abstractmethod
     async def get_by_query_one_or_none(self, *args, **kwargs):
+        raise NotImplementedError
+    
+    @abstractmethod
+    async def update_one_or_create_new(self, *args, **kwargs):
         raise NotImplementedError
 
 
@@ -18,7 +22,17 @@ class SqlAlchemyRepository(AbstractRepository):
     def __init__(self, session: AsyncSession):
         self.session = session
     
-    async def get_by_query_one_or_none(self, *args, **kwargs):
+    async def get_by_query_one_or_none(self, *args, **kwargs) -> type(model) | None:
         query = select(self.model).filter_by(**kwargs)
         res: Result = await self.session.execute(query)
         return res.unique().scalar_one_or_none()
+    
+    async def update_one_or_create_new(self, filters: dict, values: dict) -> type(model):
+        query = select(self.model).filter_by(**filters)
+        _obj: Result | None = await self.session.execute(query)
+        if _obj.scalar_one_or_none() is None:
+            stmt = insert(self.model).values(**values).returning(self.model)
+        else:
+            stmt = update(self.model).filter_by(**filters).values(**values).returning(self.model)
+        _obj: Result | None = await self.session.execute(stmt)
+        return _obj.scalar_one_or_none()
--- src/core/utils/service.py
+++ src/core/utils/service.py
@@ -15,3 +15,14 @@ class BaseService:
         async with uow:
             _obj: Base = await uow.__dict__[cls.repository].get_by_query_one_or_none(**kwargs)
             return _obj
+    
+    @classmethod
+    async def update_one_or_create_new(
+        cls,
+        uow: UnitOfWork,
+        filters: dict,
+        values: dict
+    ) -> Base | None:
+        async with uow:
+            _obj: Base = await uow.__dict__[cls.repository].update_one_or_create_new(filters=filters, values=values)
+            return _obj
