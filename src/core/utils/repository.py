e4a26257747b680d07a3a632a8234b7405eba0f2
Marsel <buzzakka6@gmail.com>
2024-04-23 21:46:06 +0300

    feat: Добавил метод get_by_query_one_or_none в репозиторий и сервис

--- src/core/utils/repository.py
+++ src/core/utils/repository.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
 
+from sqlalchemy import Result, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class AbstractRepository(ABC):
-    pass
+    
+    @abstractmethod
+    async def get_by_query_one_or_none(self, *args, **kwargs):
+        raise NotImplementedError
 
 
 class SqlAlchemyRepository(AbstractRepository):
@@ -13,3 +17,8 @@ class SqlAlchemyRepository(AbstractRepository):
 
     def __init__(self, session: AsyncSession):
         self.session = session
+    
+    async def get_by_query_one_or_none(self, *args, **kwargs):
+        query = select(self.model).filter_by(**kwargs)
+        res: Result = await self.session.execute(query)
+        return res.unique().scalar_one_or_none()
--- src/core/utils/service.py
+++ src/core/utils/service.py
@@ -1,6 +1,17 @@
-from src.core.utils.repository import AbstractRepository
+from src.core.models.base import Base
+from src.core.utils.unit_of_work import UnitOfWork
 
 
-class BaseRepository:
+class BaseService:
 
-    repository: AbstractRepository
+    repository: str
+    
+    @classmethod
+    async def get_by_query_one_or_none(
+        cls,
+        uow: UnitOfWork,
+        **kwargs
+    ) -> Base | None:
+        async with uow:
+            _obj: Base = await uow.__dict__[cls.repository].get_by_query_one_or_none(**kwargs)
+            return _obj
