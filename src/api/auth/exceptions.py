dd43343c6c6caa6c557a1461de4d1c1347cbfffe
Marsel <buzzakka6@gmail.com>
2024-04-25 17:52:23 +0300

    feat: добавил файл с исключениями

---
+++ src/api/auth/exceptions.py
@@ -0,0 +1,29 @@
+from fastapi import HTTPException, status
+
+
+def account_already_registered():
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail='Пользователь уже зарегистрирован.'
+    )
+
+
+def incorrect_account_or_invite_token():
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail='Неверный адрес электронной почты или токен.'
+    )
+
+def account_is_not_confirmed():
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail='Адрес аэлектронной почты не подтвержден.'
+    )
+
+
+def incorrect_email_or_password():
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail='Неверный логин или пароль.'
+    )
+
