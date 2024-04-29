92532750d1fb0d01fa8ddecb3b61f30935ddbf24
Marsel <buzzakka6@gmail.com>
2024-04-29 22:36:39 +0300

    feat: добавлены тесты для /sign-up

--- tests/fakes/parameters/auth.py
+++ tests/fakes/parameters/auth.py
@@ -1,8 +1,11 @@
 from contextlib import nullcontext as does_not_raise
-from fastapi import status, HTTPException
-import pytest
+from fastapi import status
 
-from src.api.auth.v1.schemas import CheckAccountResponseSchema
+from src.api.auth.v1.schemas import (
+    CheckAccountResponseSchema,
+    SignUpRequestSchema,
+    SignUpResponseSchema,
+)
 
 
 TEST_ENDPOINT_CHECK_ACCOUNT: list[tuple[any]] = [
@@ -13,21 +16,40 @@ TEST_ENDPOINT_CHECK_ACCOUNT: list[tuple[any]] = [
         does_not_raise(),
     ),
     (
-        'user_1@example.com',
-        CheckAccountResponseSchema(account='user_1@example.com').model_dump(),
-        status.HTTP_200_OK,
-        does_not_raise(),
-    ),
-    (
-        'user_2@example.com',
-        CheckAccountResponseSchema(account='user_2@example.com').model_dump(),
-        status.HTTP_200_OK,
-        does_not_raise(),
-    ),
-    (
         'user_3@example.com',
         {'detail': 'Пользователь уже зарегистрирован.'},
         status.HTTP_400_BAD_REQUEST,
         does_not_raise(),
     ),
+]
+
+TEST_ENDPOINT_SIGN_UP: list[tuple[any]] = [
+    (
+        SignUpRequestSchema(account='user_1@example.com', invite_token='123456').model_dump_json(),
+        SignUpResponseSchema(account='user_1@example.com').model_dump(),
+        status.HTTP_200_OK,
+        does_not_raise(),
+    ),
+    (
+        SignUpRequestSchema(account='user_3@example.com', invite_token='123456').model_dump_json(),
+        {'detail': 'Пользователь уже зарегистрирован.'},
+        status.HTTP_400_BAD_REQUEST,
+        does_not_raise(),
+    ),
+    (
+        SignUpRequestSchema(account='user_2@example.com', invite_token='000000').model_dump_json(),
+        {'detail': 'Неверный адрес электронной почты или токен.'},
+        status.HTTP_400_BAD_REQUEST,
+        does_not_raise(),
+    ),
+    (
+        SignUpRequestSchema(account='error@example.com', invite_token='000000').model_dump_json(),
+        {'detail': 'Неверный адрес электронной почты или токен.'},
+        status.HTTP_400_BAD_REQUEST,
+        does_not_raise(),
+    ),
+]
+
+TEST_ENDPOINT_SIGN_UP_COMPLETE: list[tuple[any]] = [
+        
 ]
--- tests/integration_tests/api/v1/test_auth.py
+++ tests/integration_tests/api/v1/test_auth.py
@@ -2,7 +2,10 @@ from fastapi import Response, status
 from fastapi.testclient import TestClient
 import pytest
 
-from tests.fakes.parameters.auth import TEST_ENDPOINT_CHECK_ACCOUNT
+from tests.fakes.parameters.auth import (
+    TEST_ENDPOINT_CHECK_ACCOUNT,
+    TEST_ENDPOINT_SIGN_UP,
+)
 
 
 class TestAuthRouterV1:
@@ -11,7 +14,7 @@ class TestAuthRouterV1:
         "email, expected_result, expected_status, expectation",
         TEST_ENDPOINT_CHECK_ACCOUNT
     )
-    def test_sign_up(
+    def test_check_account(
         self,
         client: TestClient,
         email, expected_result, expected_status, expectation
@@ -23,5 +26,23 @@ class TestAuthRouterV1:
             
             assert response.status_code == expected_status
             
-            # if response.status_code == status.HTTP_200_OK:
+            assert response.json() == expected_result
+
+    @pytest.mark.parametrize(
+        "data, expected_result, expected_status, expectation",
+        TEST_ENDPOINT_SIGN_UP
+    )
+    def test_sign_up(
+        self,
+        client: TestClient,
+        data, expected_result, expected_status, expectation,
+    ):
+        with expectation:
+            response: Response = client.post(
+                '/api/v1/auth/sign-up/',
+                data=data
+            )
+            
+            assert response.status_code == expected_status
+            
             assert response.json() == expected_result
