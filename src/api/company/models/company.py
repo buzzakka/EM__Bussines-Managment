604320cf7e50f24bc6fa6d4f9f62f02b7d54e639
Marsel <buzzakka6@gmail.com>
2024-05-01 23:19:09 +0300

    refactor: удалил лишние схемы

--- src/api/company/models/company.py
+++ src/api/company/models/company.py
@@ -1,7 +1,5 @@
-from copy import deepcopy
 from sqlalchemy.orm import Mapped
 
-from src.api.company.schemas import CompanySchema
 from src.core.models.base import Base
 from src.core.models.mixins.custom_types import (
     int_pk_T,
@@ -17,8 +15,3 @@ class CompanyModel(Base):
     name: Mapped[str]
     created_at: Mapped[created_at_T]
     updated_at: Mapped[updated_at_T]
-
-    def to_pydantic_schema(self):
-        dict_copy: dict = deepcopy(self.__dict__)
-        dict_copy.pop('_sa_instance_state')
-        return CompanySchema(**dict_copy)
--- src/api/company/models/member.py
+++ src/api/company/models/member.py
@@ -1,8 +1,6 @@
-from copy import deepcopy
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from src.api.company.schemas import MemberSchema
 from src.core.models.base import Base
 from src.core.models.mixins.custom_types import int_pk_T
 
@@ -17,8 +15,3 @@ class MemberModel(Base):
 
     account: Mapped['AccountModel'] = relationship()
     company: Mapped['CompanyModel'] = relationship()
-    
-    def to_pydantic_schema(self):
-        dict_copy: dict = deepcopy(self.__dict__)
-        dict_copy.pop('_sa_instance_state')
-        return MemberSchema(**dict_copy)
