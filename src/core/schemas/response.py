6de2997e3a2d6592f6cfe1c116b0fd446d3aef87
Marsel <buzzakka6@gmail.com>
2024-05-07 10:59:52 +0300

    feat: Добавил стандатртную схему ответа

--- src/core/schemas/response.py
+++ src/core/schemas/response.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
+from fastapi import status
 
 
-class BaseResponseModel:
-    status_code: int
-    error: bool
-    payload: dict
+class BaseResponseModel(BaseModel):
+    status_code: int = status.HTTP_200_OK
+    error: bool = False
+    payload: dict | None = None
+    message: str | None = None
